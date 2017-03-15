62c52cefa2a2c48d4c345a2818259cf572ecb4b5
Xalalau Xubilozo <xalalau@outlook.com>
2017-03-15 12:20:13 -0300

    Velocidade de puxada da samambaia aumentada

--- game/server/entities/NPCs/CBarnacle.h
+++ game/server/entities/NPCs/CBarnacle.h
@@ -16,7 +16,10 @@
 #define GAME_SERVER_ENTITIES_NPCS_CBARNACLE_H
 
 #define	BARNACLE_BODY_HEIGHT	44 // how 'tall' the barnacle's model is.
-#define BARNACLE_PULL_SPEED		8
+// ############ hu3lifezado ############ //
+// Agora a puxada e bem mais rapida. Muito mais emocao! (8)
+#define BARNACLE_PULL_SPEED		30
+// ############ //
 #define BARNACLE_KILL_VICTIM_DELAY	5 // how many seconds after pulling prey in to gib them. 
 
 //=========================================================
