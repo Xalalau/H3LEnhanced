d073d89f3c6e3e2afaac54278c01ff5d974a6710
Xalalau Xubilozo <xalalau@outlook.com>
2017-03-19 16:01:34 -0300

    Adicionado monster_scientist_gigante

---
+++ game/server/entities/NPCs/CScientistGiant.cpp
@@ -0,0 +1,75 @@
+// ##############################
+// HU3-LIFE CIENTISTA GIGANTE
+// ##############################
+
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "Monsters.h"
+#include "CTalkMonster.h"
+#include "Schedule.h"
+#include "DefaultAI.h"
+#include "animation.h"
+#include "entities/CSoundEnt.h"
+#include "CScientist.h"
+
+#include "CScientistGiant.h"
+
+LINK_ENTITY_TO_CLASS(monster_scientist_gigante, CScientistGiant);
+
+//=========================================================
+// Spawn
+//=========================================================
+void CScientistGiant::Spawn(void)
+{
+	Precache();
+
+	SetModel("models/scientist_gigante.mdl");
+	SetSize(VEC_HUMAN_HULL_MIN, VEC_HUMAN_HULL_MAX);
+
+	pev->solid = SOLID_SLIDEBOX;
+	pev->movetype = MOVETYPE_STEP;
+	m_bloodColor = BLOOD_COLOR_RED;
+	pev->health = gSkillData.GetScientistHealth();
+	pev->view_ofs = Vector(0, 0, 50);// position of the eyes relative to monster's origin.
+	m_flFieldOfView = VIEW_FIELD_WIDE; // NOTE: we need a wide field of view so scientists will notice player and say hello
+	m_MonsterState = MONSTERSTATE_NONE;
+
+	//	m_flDistTooFar		= 256.0;
+
+	m_afCapability = bits_CAP_HEAR | bits_CAP_TURN_HEAD | bits_CAP_OPEN_DOORS | bits_CAP_AUTO_DOORS | bits_CAP_USE;
+
+	// White hands
+	pev->skin = 0;
+
+	if (pev->body == -1)
+	{// -1 chooses a random head
+		pev->body = RANDOM_LONG(0, NUM_SCIENTIST_HEADS - 1);// pick a head, any head
+	}
+
+	// Luther is black, make his hands black
+	if (pev->body == HEAD_LUTHER)
+		pev->skin = 1;
+
+	MonsterInit();
+	SetUse(&CScientist::FollowerUse);
+}
+
+//=========================================================
+// Precache - precaches all resources this monster needs
+//=========================================================
+void CScientistGiant::Precache(void)
+{
+	PRECACHE_MODEL("models/scientist_gigante.mdl");
+	PRECACHE_SOUND("scientist/sci_pain1.wav");
+	PRECACHE_SOUND("scientist/sci_pain2.wav");
+	PRECACHE_SOUND("scientist/sci_pain3.wav");
+	PRECACHE_SOUND("scientist/sci_pain4.wav");
+	PRECACHE_SOUND("scientist/sci_pain5.wav");
+
+	// every new scientist must call this, otherwise
+	// when a level is loaded, nobody will talk (time is reset to 0)
+	TalkInit();
+
+	CTalkMonster::Precache();
+}
