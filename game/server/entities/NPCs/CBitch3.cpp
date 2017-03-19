7e277bb9ea5be39e8b6c6923477c962d2f34ff69
Xalalau Xubilozo <xalalau@outlook.com>
2017-03-19 17:27:31 -0300

    Adicionados monster_puta_2 ao 4

---
+++ game/server/entities/NPCs/CBitch3.cpp
@@ -0,0 +1,47 @@
+// ##############################
+// HU3-LIFE PUTA 3
+// ##############################
+
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "Monsters.h"
+#include "CTalkMonster.h"
+#include "Schedule.h"
+#include "DefaultAI.h"
+#include "animation.h"
+#include "entities/CSoundEnt.h"
+#include "CScientist.h"
+#include "CBitch1.h"
+
+#include "CBitch3.h"
+
+LINK_ENTITY_TO_CLASS(monster_puta_3, CBitch3);
+
+//=========================================================
+// Spawn
+//=========================================================
+void CBitch3::Spawn(void)
+{
+	PRECACHE_MODEL("models/puta3.mdl");
+	Precache();
+
+	SetModel("models/puta3.mdl");
+	SetSize(VEC_HUMAN_HULL_MIN, VEC_HUMAN_HULL_MAX);
+
+	pev->solid = SOLID_SLIDEBOX;
+	pev->movetype = MOVETYPE_STEP;
+	m_bloodColor = BLOOD_COLOR_RED;
+	pev->health = gSkillData.GetScientistHealth();
+
+	pev->view_ofs = Vector(0, 0, 50);// position of the eyes relative to monster's origin.
+	m_flFieldOfView = VIEW_FIELD_WIDE; // NOTE: we need a wide field of view so scientists will notice player and say hello
+	m_MonsterState = MONSTERSTATE_NONE;
+
+	//	m_flDistTooFar		= 256.0;
+
+	m_afCapability = bits_CAP_HEAR | bits_CAP_TURN_HEAD | bits_CAP_OPEN_DOORS | bits_CAP_AUTO_DOORS | bits_CAP_USE;
+
+	MonsterInit();
+	SetUse(&CScientist::FollowerUse);
+}
