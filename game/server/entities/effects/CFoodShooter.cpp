982305a7bb622efa96aebace37c3e9994bf589a5
Xalalau Xubilozo <xalalau@outlook.com>
2017-03-19 13:29:47 -0300

    foodshooter implementado

---
+++ game/server/entities/effects/CFoodShooter.cpp
@@ -0,0 +1,34 @@
+// ##############################
+// HU3-LIFE LANCADOR DE COMIDAS
+// ##############################
+
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "CGib.h"
+#include "CGibShooter.h"
+
+#include "CFoodShooter.h"
+
+LINK_ENTITY_TO_CLASS(foodshooter, CFoodShooter);
+
+void CFoodShooter::Precache(void)
+{
+	m_iGibModelIndex = PRECACHE_MODEL("models/fgibs.mdl");
+}
+
+CGib *CFoodShooter::CreateGib(void)
+{
+	CGib *pGib = GetClassPtr((CGib *)NULL);
+	pGib->Spawn("models/fgibs.mdl");
+	pGib->m_bloodColor = BLOOD_COLOR_YELLOW;
+
+	if (pev->body <= 1)
+	{
+		ALERT(at_aiconsole, "FoodShooter Body is <= 1!\n");
+	}
+
+	pGib->pev->body = RANDOM_LONG(0, pev->body - 1);
+
+	return pGib;
+}
