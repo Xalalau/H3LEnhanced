9813696f49e756de76ccb94324487e8ffe083c62
Xalalau Xubilozo <xalalau@outlook.com>
2019-07-19 15:23:22 -0300

    Adicionado monster_generic_god

---
+++ game/server/entities/NPCs/CGenericMonsterGod.h
@@ -0,0 +1,28 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+#ifndef GAME_SERVER_ENTITIES_NPCS_CGENERICMONSTERGOD_H
+#define GAME_SERVER_ENTITIES_NPCS_CGENERICMONSTERGOD_H
+
+//=========================================================
+// Monster's Anim Events Go Here
+//=========================================================
+
+class CGenericMonsterGod : public CGenericMonster
+{
+public:
+	void Spawn(void);
+};
+
+#endif //GAME_SERVER_ENTITIES_NPCS_CGENERICMONSTERGOD_H
