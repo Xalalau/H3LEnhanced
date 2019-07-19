b9a8e197e3b2bceab3dc1135fed315b17c35e874
Xalalau Xubilozo <xalalau@outlook.com>
2019-07-19 15:27:30 -0300

    Adicionado monster_scientist_falsificado

---
+++ game/server/entities/NPCs/CScientistParaguai.h
@@ -0,0 +1,33 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+#ifndef GAME_SERVER_ENTITIES_NPCS_CSCIENTISTPARAGUAI_H
+#define GAME_SERVER_ENTITIES_NPCS_CSCIENTISTPARAGUAI_H
+
+class CScientistParaguai : public CScientist
+{
+public:
+	DECLARE_CLASS( CScientistParaguai, CScientist );
+	DECLARE_DATADESC();
+
+	void Spawn(void);
+	void Precache(void);
+
+private:
+	float m_painTime;
+	float m_healTime;
+	float m_fearTime;
+};
+
+#endif //GAME_SERVER_ENTITIES_NPCS_CSCIENTISTPARAGUAI_H
